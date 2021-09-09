3549bedbd06e1f2bf6e92f4bc46cbfaaf48d7b83
Joel Tinoco <wizard.joel@gmail.com>
2021-09-08 22:07:23 -0500

    feat: improvements in works page, added links and pictures

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -15,24 +15,53 @@ const Works = () => {
       <div className="section_content">
         <div className="projects">
 
-          <div>
-            <p>Aplicativo móvil Auna</p>
+          <div className="work">
+            <div>
+              <p>Website CSJJunin</p>
+            </div>
+            <div className="picture">
+              <a href="http://csjjunin.gob.pe/" target="_blank">
+                <img src="/assets/img/web_corte_junin.jpg" alt="aweb corte junin" loading="lazy" />
+              </a>
+            </div>
           </div>
 
-          <div>
-            <p>Portal Web CSJJunin</p>
+          <div className="work">
+            <div>
+              <p>Application CSJJunin (Playstore)</p>
+            </div>
+            <div className="picture">
+              <a href="https://play.google.com/store/apps/details?id=pe.gob.csjjunin.app.cortejunin" target="_blank">
+                <img src="/assets/img/app_corte_junin.jpg" alt="app corte junin" loading="lazy" />
+              </a>
+            </div>
           </div>
 
-          <div>
-            <p>Aplicativo móvil CSJJunin</p>
+          <div className="work">
+            <div>
+              <p>Application IMC Calculator (AppGallery)</p>
+            </div>
+            <div className="picture">
+              <a href="#">
+                <img src="/assets/img/imc_calculator.jpg" alt="app corte junin" loading="lazy" />
+              </a>
+            </div>
           </div>
 
-          <div>
-            <p>Sopator</p>
+          <div className="work">
+            <div>
+              <a className="anchor" href="http://sopator.260mb.net/" target="_blank">
+                <p>Sopator</p>
+              </a>
+            </div>
           </div>
 
-          <div>
-            <p>GoCruz</p>
+          <div className="work">
+            <div>
+              <a className="anchor" href="https://github.com/JoelTinx/GoCruz" target="_blank">
+                <p>GoCruz</p>
+              </a>
+            </div>
           </div>
       
         </div>
